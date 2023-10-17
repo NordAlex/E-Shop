ae9a6506ff4fc6e3562d0339b1168a261d2b8e4a
Alexander Olshansky <olshansky.work@gmail.com>
2023-10-17 22:44:08 +0300

    Clean up

--- Eshop.Catalog/EShop.Catalog.Application/Categories/Queries/GetCategories/CategoryItemDto.cs
+++ Eshop.Catalog/EShop.Catalog.Application/Categories/Queries/GetCategories/CategoryItemDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EShop.Catalog.Application.Categories.Queries.GetCategories
+﻿namespace EShop.Catalog.Application.Categories.Queries.GetCategories
 {
     public class CategoryItemDto
     {
